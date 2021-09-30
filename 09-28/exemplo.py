091b229c0f59bdb1fd9a9fa1dc495f542cdfae4d
Mario H. Adaniya <mhadaniya@gmail.com>
2021-09-30 20:55:33 -0300

    :memo: subindo o codigo da aula do dia 30/09

---
+++ 09-28/exemplo.py
@@ -0,0 +1,53 @@
+# a = 10.0
+# b = 'vamos la'
+# # b -> [v][a][m][o][s][][l][a]
+# # b -> [0][1][2][3][4][5[6][7]
+# c = a + b
+class Lista:
+    """Exemplo de uma classe Lista
+    """
+
+    def __init__(self, items):
+        self.items = items
+
+    def add(self, item):
+        self.items.append(item)
+    
+
+    def contains(self, item):
+        """Função para verificar se um item existe na lista.
+
+        Args:
+            item ([type]): Item que quero saber se existe
+
+        Returns:
+            boolean: `True` se existe, `False` se não existe.
+        """
+        achou = False
+
+        for elem in c:        
+            if (elem == item):
+                achou = True
+                break
+
+        return achou
+
+
+c = [1, 9, 5, 32, 27, 81, 16, 33]
+
+lista = Lista(c)
+
+print(lista)
+
+for e in lista.items:
+    print(e)
+
+y = int(input('Qual o n?'))
+
+print(lista.contains(y))
+
+
+# if (achou):
+#     print('achou')
+# else:
+#     print('não achou')
