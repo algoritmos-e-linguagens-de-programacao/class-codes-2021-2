c0a6f572b998f54cc265916727bc8004542a05da
Mario H. Adaniya <mhadaniya@gmail.com>
2021-11-09 22:12:38 -0300

    :sparkles: listas encadeadas

--- tad-lista/lista.py
+++ tad-lista/lista.py
@@ -4,29 +4,41 @@ class Node:
     def __init__(self, x):
         self.x = x
         self.next = None
+        self.prev = None
 
 
 class Lista:
 
     def __init__(self):
         self.init = None
+        self.tail = None
 
     def append(self, node):
+        """
+        Método para inserir um elemento no final
 
+        :param node:
+        :return:
+        """
         if self.init is None:
             self.init = node
+            self.tail = node
             return
 
-        node_aux = self.init
-        while node_aux.next is not None:
-            node_aux = node_aux.next
+        self.tail.next = node
+        node.prev = self.tail
 
-        node_aux.next = node
 
     def add(self, node):
+        """
+        Inserir um elemento sempre no inicio da lista
 
+        :param node:
+        :return:
+        """
         if self.init is None:
             self.init = node
+            self.tail = node
             return
 
         node.next = self.init
@@ -44,10 +56,9 @@ class Lista:
 
 if __name__ == '__main__':
     lista = Lista()
+    lista.add(Node(x=27))
+    lista.add(Node(x=1))
+    print(lista)
     lista.append(Node(x=5))
     lista.append(Node(x=19))
-    print(lista)
-    lista.add(Node(x=27))
-    print(lista)
-    lista.add(Node(x=1))
     print(lista)
